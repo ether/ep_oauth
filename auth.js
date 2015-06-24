2cc6c7e6fd8b31c1d0a02f14fc6b2d542a4fa841
John McLear <john@mclear.co.uk>
2015-06-24 22:01:50 +0100

    working version without the nastiness that is passport

--- auth.js
+++ auth.js
@@ -6,6 +6,7 @@ var sessionStore = require('ep_etherpad-lite/node/db/SessionStore');
 
 var OAuth2 = require('./node_modules/oauth/lib/oauth2').OAuth2;
 
+// Setup the oauth2 connector -- Doesn't establish any connections etc.
 var oauth2 = new OAuth2(settings.ep_oauth.clientID,
   settings.ep_oauth.clientSecret,
   'https://github.com/', 
@@ -15,75 +16,88 @@ var oauth2 = new OAuth2(settings.ep_oauth.clientID,
 
 exports.expressConfigure = function(hook_name, args, cb) {
 
-  
-/*
+  // args.app.get('/auth/callback', function(req, res){
+
+  // THIRD STEP
   args.app.use(function(req, res, next) {
+    // Oauth2 Provider returns auth code so that access_token can be obtained 
 
-    if(req.path.indexOf("/login") === 0){
-      console.log("Trying to access a pad", req.path);
-
-      var authURL = oauth2.getAuthorizeUrl({
-        redirect_uri: settings.ep_oauth.callbackURL,
-        scope: ['repo', 'user'],
-        state: 'herpDERPCHANGEMEINSETTINGS'
-      });
-
-      var body = '<a href="' + authURL + '"> Get Code </a>';
-      res.writeHead(200, {
-       'Content-Length': body.length,
-       'Content-Type': 'text/html' });
-      res.end(body);
- 
-      // console.warn(authURL);
-      return next();
-    }else{
-      return next();
-    }
-
-  });
-*/
-
-  args.app.get('/auth/callback', function(req, res){
-    /** Github sends auth code so that access_token can be obtained */
-    /** Obtaining access_token */
-    oauth2.getOAuthAccessToken(
+    if(req.url.indexOf("/auth/callback") === 0){
+      console.debug("/auth/callback");
+      oauth2.getOAuthAccessToken(
       req.query.code,
       {'redirect_uri': settings.ep_oauth.callbackURL},
       function (e, access_token, refresh_token, results){
-      if (e) {
-        console.log(e);
-        res.end(e);
-      } else if (results.error) {
-        console.log(results);
-        res.end(JSON.stringify(results));
-      }
-      else {
-        console.log('Obtained access_token: ', access_token);
-        req.session.ep_oauth = access_token;
-        // I need to figure out what to do with this?  Do I store it against the users sessionID?
-        // We should store it against the user in the SessionManager and flag that user as "authenticated"
-        // Here I wanna set req.session.user to be true? 
-        // then I wanna do the check in req.authorize
-        res.end( access_token);
-      }
-    });
+        if (e) {
+          // General Error
+          console.error(e);
+          res.end(e);
+        } else if (results.error) {
+          // Error in Results
+          console.error("error in results", results);
+          // res.end(JSON.stringify(results));
+        }
+        else {
+          // Everything is all good, we have an access token for this user
+          console.debug('Obtained access_token: ', access_token);
+          var sessionID = req.query.state;
+          // We should store it against the user in the SessionManager
+          if(sessionID){
+            console.debug("Database Write -> ", sessionID, "---", access_token);
+            db.set("oauth:"+sessionID, access_token);
+            next(); // Go to final step
+          }
+        }
+      })
+    }else{
+      next(); // Go to final step
+    }
   });
 
+  // FOURTH AND FINAL STEP
+  args.app.get('/auth/callback', function(req, res){
+    // Read redirect lookup URL from database
+    db.get("oauthredirectlookup:"+req.query.state, function(k, url){
+      console.debug("Oauth redirect lookup record found", url);
+      res.redirect(url || "/"); // CAKE
+    })
+  });
 }
 
-
+// FIRST STEP
 exports.authorize = function(hook_name, args, cb){
-  console.warn("authorize args");
+  // Never lands here for url /auth/callback
+  if(args.req.url.indexOf("/auth") === 0) return cb[true];
+
   var userIsAuthedAlready = false;
-  var user = args.req.session.user;
+  console.debug("Database lookup -> oauth:"+args.req.sessionID);
+  db.get("oauth:"+args.req.sessionID, function(k, token){
+    console.debug("Oauth session found ->" + args.req.sessionID, "has token value of ", token);
+    if(token) userIsAuthedAlready = true;
+  });
   return cb([userIsAuthedAlready]);
 }
 
-exports.authenticate = function(hook_name, args, mangle){
+// SECOND STEP
+exports.authenticate = function(hook_name, args, cb){
+  console.log("Database Write -> oauthredirectlookup:"+args.req.sessionId, "---", args.req.url);
+  db.set("oauthredirectlookup:"+args.req.sessionID, args.req.url);
   // User is not authorized so we need to do the authentication step
-  if(args.req.url !== '/oauth/login'){
-  console.log(args.req);
-    args.res.redirect('/oauth/login');
-  }
+  // Gets an authoritzation URL for the user to hit..
+
+  // CAKE TODO -- we use redirect url as state, this seems wrong to me.
+  var authURL = oauth2.getAuthorizeUrl({
+    redirect_uri: settings.ep_oauth.callbackURL,
+    scope: ['repo', 'user'],
+    state: args.req.sessionID,
+    target: args.req.url
+  });
+
+  args.res.redirect(authURL);
+  // CAKE TODO -- This redirect fires a server console error because of when it's fired
+  // It might make more sense to send this redirect as a browser script or so..
+  // Let's see if it causes issues and if it does we can address it then..
+
+  return cb([true]);
 }
 
